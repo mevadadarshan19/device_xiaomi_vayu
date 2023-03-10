ac1e3cfdee5fd4166bf738ede7c57cb0576029d1
mevadadarshan19 <mevadadarshan19@gmail.com>
2023-03-10 20:43:50 +0530

    vayu:xtended bringup

---
+++ xtended_vayu.mk
@@ -0,0 +1,34 @@
+#
+# Copyright (C) 2018-2021 ArrowOS
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit common products
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit device configurations
+$(call inherit-product, device/xiaomi/vayu/device.mk)
+
+# Inherit common ArrowOS configurations
+$(call inherit-product, vendor/xtended/config/common_full_phone.mk)
+
+# Optionals
+TARGET_BOOT_ANIMATION_RES := 1080e
+TARGET_SUPPORTS_QUICK_TAP := true
+
+PRODUCT_NAME := xtended_vayu
+PRODUCT_DEVICE := vayu
+PRODUCT_BRAND := POCO
+PRODUCT_MODEL := Poco X3 Pro
+PRODUCT_MANUFACTURER := Xiaomi
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="vayu_global-user 12 SKQ1.211006.001 V13.0.5.0.SJUMIXM release-keys" \
+    PRODUCT_NAME=vayu_global \
+    PRODUCT_MODEL=M2102J20SI
+
+BUILD_FINGERPRINT := Xiaomi/vayu_global/vayu:12/RKQ1.200826.002/V13.0.5.0.SJUMIXM:user/release-keys
